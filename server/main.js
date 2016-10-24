8afb5fea5061a0027dd169d44c4427281c5cbf40
Joe MacMahon <joe@macmahon.eu>
2016-10-24 19:18:15 +0100

    Added logging to server

--- server/main.js
+++ server/main.js
@@ -3,10 +3,18 @@ const compression = require('compression');
 const root = require('./root');
 const api = require('./api');
 const MongoClient = require('mongodb').MongoClient;
+const morgan = require('morgan');
+const fs = require('fs');
 
 const app = express();
 
+const logStream = fs.createWriteStream('./access.log', { flags: 'a' });
+const logger = morgan('combined', {
+  stream: logStream,
+});
+
 app.use(compression());
+app.use(logger);
 app.use('/api', api.router);
 app.use('/', root.router);
 app.set('db', MongoClient.connect('mongodb://localhost:27017/modules'));
